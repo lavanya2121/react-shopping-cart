bf63a780a3c359557f14ade3e020e54f0d2c1f53
lavanya2121 <lavanya11289@gmail.com>
2020-08-13 12:01:26 +0530

    create order

--- server.js
+++ server.js
@@ -39,6 +39,50 @@ app.delete("/api/products/:id", async (req,res)=>{
     res.send(deletedProduct);
 })
 
-//craetion of express server
+const Order = mongoose.model(
+    "order",
+    new mongoose.Schema(
+      {
+        _id: {
+          type: String,
+          default: shortid.generate,
+        },
+        email: String,
+        name: String,
+        address: String,
+        total: Number,
+        cartItems: [
+          {
+            _id: String,
+            title: String,
+            price: Number,
+            count: Number,
+          },
+        ],
+      },
+      {
+        timestamps: true,
+      }
+    )
+  );
+
+  app.post("/api/orders", async (req, res) => {
+    if (
+      !req.body.name ||
+      !req.body.email ||
+      !req.body.address ||
+      !req.body.total ||
+      !req.body.cartItems
+    ) {
+      return res.send({ message: "Data is required." });
+    }
+    const order = await Order(req.body).save();
+    res.send(order);
+  });
+  app.get("/api/orders", async (req, res) => {
+    const orders = await Order.find({});
+    res.send(orders);
+  });
+  
 const port=process.env.PORT || 5000;
 app.listen(port,()=>console.log("server at http://localhost:5000"))
